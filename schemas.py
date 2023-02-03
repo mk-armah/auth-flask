098f5babf0cf4b15ef6e84f9185596a45669c9cf
mk-armah <mikeyarmah@gmail.com>
2023-02-03 13:38:52 +0000

    Init Commit

---
+++ schemas.py
@@ -0,0 +1,30 @@
+from wtforms import Form, BooleanField, StringField, PasswordField, validators, IntegerField
+
+class RegistrationForm(Form):
+    firstname =  StringField('Fistname', [validators.DataRequired()])
+
+    lastname = StringField('Lastname', [validators.DataRequired()])
+    
+    other_names = StringField('Other Names') 
+
+    email = StringField('Email Address', [validators.DataRequired(),validators.Email(message= "Invalid Email")]) 
+    
+    phone = StringField("Phone Number",validators = [validators.Length(min=6, max=20)]) 
+    password = PasswordField('New Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
+    accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()])
+
+
+    from pydantic import BaseModel
+
+
+    class RegistrationSchema(BaseModel):
+        firstname:str
+        lastname:str
+        other_names:str|None = None
+        email:str
+        phone:int
+        password:str
